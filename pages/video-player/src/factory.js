dbf4d4e355564233a7eb376b58727eeb26e02b62
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2023-03-02 00:05:33 -0300

    feat: implement worker logic on same thread for browsers that do not support modules on workers

--- pages/video-player/src/factory.js
+++ pages/video-player/src/factory.js
@@ -8,20 +8,47 @@ const [rootPath] = window.location.href.split('/pages/')
 
 async function getWorker() {
   if (supportsWorkerType()) {
+    console.log('initialize esm workers')
     const worker = new Worker('./src/worker.js', { type: 'module' })
     return worker
   }
 
+  console.warn('your browser does not support workers')
+  console.warn('importing libraries')
+
+  await import("https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js")
+  await import("https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js")
+  await import("https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js")
+  await import("https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js")
+
+  console.warn('using worker mock instead')
+
+  const service = new Service({
+    faceLandmarksDetection: window.faceLandmarksDetection
+  })
+
   const workerMock = {
-    async postMessage() { },
+    async postMessage(video) {
+      const blinked = await service.handBlinked(video)
+      if (!blinked) return
+      workerMock.onMessage({ data: blinked })
+    },
+    // it will be overwritten by the controller
     onmessage(msg) { }
   }
-  console.log('this browser does not support workers')
+
+  console.log('loading tf model')
+  await service.loadModel()
+  console.log('tf model loaded')
+
+  setTimeout(() => {
+    worker.onmessage({ data: 'READY' })
+  }, 500)
   return workerMock
 }
 
-const worker = await getWorker()
 const camera = await Camera.init()
+const worker = await getWorker()
 
 const factory = {
   async initalize() {
