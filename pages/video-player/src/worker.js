942867379c9a09583416c60a3c343123ccb4afe6
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2023-11-03 11:08:45 -0300

    perf: improve performance by using cpu

--- pages/video-player/src/worker.js
+++ pages/video-player/src/worker.js
@@ -1,6 +1,7 @@
 import "https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js"
 import "https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js"
 import "https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js"
+import "https://unpkg.com/@tensorflow/tfjs-backend-cpu@2.4.0/dist/tf-backend-cpu.js"
 import "https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js"
 
 import Service from "./service.js"
